050e7c222ceff146bf9af2161eaeb867dd7e1dd4
PacDraft <Kirsten@PacificDraftWorks.com>
2021-12-05 13:37:09 -0800

    added utils file & some questions in index

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+                } else{
+                    console.log('Please enter the name of your project!');
+                    return false;
+                }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
