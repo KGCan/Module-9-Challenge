7a96ec5a7890f7f8d4ae8e2624b1538fcc3a2b58
PacDraft <Kirsten@PacificDraftWorks.com>
2021-12-09 14:28:33 -0800

    added link for markdown to index and added functionality to generatemarkdown file

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
+const generateMarkdown = require ('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = () => {
+    return inquirer.prompt([
     {
         type: 'input',
-        name: 'project',
+        name: 'projectTitle',
         message: 'What is the name of your project? (Required)',
         validate: nameInput => {if (nameInput){return true} else{console.log('Please enter the name of your project!'); return false;}}
     },
     {
         type: 'input',
-        name: 'Description',
-        message: 'Please provide a description of your project: (Required)',
+        name: 'description',
+        message: 'Please provide a description of your project:',
         validate: nameInput => {if (nameInput){return true} else{console.log('You must enter a description of your project!'); return false;}}
     },
     {
         type: 'input',
         name: 'installation',
-        message: 'Please provide instructions for installation: (Required)',
+        message: 'What command should be used to install dependencies?:',
+        default: 'npm i',
         validate: nameInput => {if (nameInput){return true} else{console.log('Please enter instructions!'); return false;}}
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'How do you use your app? (Required)',
+        message: 'How do you use your repo? (Required)',
         validate: nameInput => {if (nameInput){return true} else{console.log('Please enter directions for usage!'); return false;}}
     },
     {
@@ -38,12 +42,12 @@ const questions = [
         type: 'list',
         name: 'license',
         message: 'Which license(s) were used? (Required)',
-        choices: ['The MIT license', 'The GPL License', 'Apache License'],
+        choices: ['MIT', 'GPL', 'Apache', 'none'],
         validate: nameInput => {if (nameInput){return true} else{console.log('Please select at least one!'); return false;}}
     },
     {
         type: 'input',
-        name: 'GitHub',
+        name: 'github',
         message: 'What is your GitHub username? (Required)',
         validate: nameInput => {if (nameInput){return true} else{console.log('Please enter your username!'); return false;}}
     },
@@ -52,8 +56,9 @@ const questions = [
         name: 'email',
         message: 'What is your email? (Required)',
         validate: nameInput => {if (nameInput){return true} else{console.log('Please enter an email!'); return false;}}
-    },
-];
+    }
+]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,76 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'none') {
+    return `![GitHub License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'None') {
+    return `\n* [License](#license)\n`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return `## License
+
+This project is licensed under ${license}.`;
+  }
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
+  ${renderLicenseBadge(data.license)}
 
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents 
+  
+  * [installation](#installation)
+  
+  * [usage](#usage)
+  ${renderLicenseLink(data.license)}
+  * [contributions](#contributions)
+  
+  * [Questions](#questions)
+  
+  ## Installation
+  
+  To install necessary dependencies, run the following command:
+  
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ${renderLicenseSection(data.license)}
+    
+  ## Contributions
+  
+  ${data.contributions}
+  
+  ## Questions
+  
+  If you have any questions about this repo, please open an issue or contact me directly at ${
+      data.email
+    }. You can find more of my work at [${data.github}](https://github.com/${
+      data.github
+    }/).
 `;
 }
 
